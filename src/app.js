5275ddc314183cb5d30df70d2241ffa55a19d11a
dkokolakis <dkokolakis@telesto.gr>
2021-09-10 10:18:40 +0300

    adding swagger configuration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,42 @@
 const express = require('express')
-const app = express()
+const swaggerJSDoc = require('swagger-jsdoc');  
+const swaggerUI = require('swagger-ui-express');  
 const treasuryRouter = require('./routes/treasuryRouter')
 const globalErrorHandler = require('./controllers/errorController');
+const path = require('path')
+
+const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+
+//Swagger Configuration  
+const swaggerOptions = {  
+    swaggerDefinition: {  
+        openapi: '3.0.0',
+        info: {  
+            title:'i3Treasury API',  
+            version:'1.0.0',
+            license: {
+                name: 'Licensed Under MIT',
+                url: 'https://spdx.org/licenses/MIT.html',
+              }, 
+        },
+        servers: [
+            {
+              url: 'http://localhost:3001',
+              description: 'Development server',
+            },
+        ],
+        
+    },
+    apis:[path.join(__dirname,'./routes/**.js')],
+    
+}  
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);  
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs)); 
 
 app.use('/api/v1/treasury', treasuryRouter);
 
