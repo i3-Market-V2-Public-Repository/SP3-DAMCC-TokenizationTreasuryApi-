9fba7f7ca456f733f4869abf967a9963d215e4ba
dkokolakis <dkokolakis@telesto.gr>
2021-11-23 16:16:26 +0200

    class for handle errors

---
+++ src/utils/AppError.js
@@ -0,0 +1,26 @@
+/**
+* Copyright (c) 2020-2022 in alphabetical order:
+* GFT, Telesto Technologies
+*
+* This program and the accompanying materials are made
+* available under the terms of the MIT,Apache License 2.0,ISC
+* which is available at https://github.com/panva/jose/blob/main/LICENSE.md
+*
+* License-Identifier: EUPL-2.0
+*
+* Contributors:
+*    Vangelis Giannakosian (Telesto Technologies)
+*    Dimitris Kokolakis (Telesto Technologies)
+*
+*/
+
+class AppError extends Error {  
+    constructor(message, statusCode) {    
+    super(message);
+    this.statusCode = statusCode;    
+    this.status = `${statusCode}`.startsWith('4') ? 'rejected' : 'error';    
+    Error.captureStackTrace(this, this.constructor);  
+    }
+}
+
+module.exports = AppError;
