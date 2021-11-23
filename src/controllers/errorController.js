8f1b2fc5ea1d63b29428ce8215af00d1343c2acf
dkokolakis <dkokolakis@telesto.gr>
2021-11-23 16:15:53 +0200

    add copyrigth and minor changes

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,3 +1,19 @@
+/**
+* Copyright (c) 2020-2022 in alphabetical order:
+* GFT, Telesto Technologies
+*
+* This program and the accompanying materials are made
+* available under the terms of the MIT,Apache License 2.0,ISC
+* which is available at https://github.com/panva/jose/blob/main/LICENSE.md
+*
+* License-Identifier: EUPL-2.0
+*
+* Contributors:
+*    Vangelis Giannakosian (Telesto Technologies)
+*    Dimitris Kokolakis (Telesto Technologies)
+*
+*/
+
 module.exports = (err, req, res, next) => {
 
     err.statusCode = err.statusCode || 500;
@@ -5,8 +21,6 @@ module.exports = (err, req, res, next) => {
 
     return res.status(err.statusCode).json({
         status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack
+        message: err.message
     });
 };
