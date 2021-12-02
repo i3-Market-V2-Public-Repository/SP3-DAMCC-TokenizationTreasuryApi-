0130618e8ef743e68ebe82cb5fa827deda7d927b
dkokolakis <dkokolakis@telesto.gr>
2021-12-02 13:43:32 +0200

    change license in copyright notice

--- src/services/TreasuryContractService.js
+++ src/services/TreasuryContractService.js
@@ -3,10 +3,10 @@
 * GFT, Telesto Technologies
 *
 * This program and the accompanying materials are made
-* available under the terms of the MIT
+* available under the terms of the EUROPEAN UNION PUBLIC LICENCE v. 1.2
 * which is available at https://gitlab.com/i3-market/code/wp3/t3.3/nodejs-tokenization-treasury-api/-/blob/master/LICENCE.md
 *
-* License-Identifier: MIT
+* License-Identifier: EUPL-1.2
 *
 * Contributors:
 *    Vangelis Giannakosian (Telesto Technologies)
